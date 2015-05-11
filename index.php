0837b0942eddfc0c9faaca266e4c8147b6d6de73
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2015-05-11 06:02:52 +0300

    turned on error reporting

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 require_once(dirname(__FILE__) . '/lib/auth.php');
 require_once(dirname(__FILE__) . '/lib/mysql.php');
 
