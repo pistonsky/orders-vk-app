607980d510b29c717f8b5d5875d360612da7f9ed
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2015-07-12 22:23:09 +0300

    mysql prod settings

--- lib/mysql.php
+++ lib/mysql.php
@@ -1,20 +1,20 @@
 <?php
 
-$users_db = mysqli_connect("localhost","gb_orders1","e352792dyzx","gb_orders1",8889);
+$users_db = mysqli_connect("mysql87.1gb.ru","gb_orders1","e352792dyzx","gb_orders1");
 if (!$users_db) {
     die('Connect Error (' . mysqli_connect_errno() . ') '
             . mysqli_connect_error());
 }
 mysqli_query($users_db, 'SET NAMES utf8');
 
-$orders_db = mysqli_connect("localhost","gb_orders2","za84ededawps","gb_orders2",8889);
+$orders_db = mysqli_connect("mysql87.1gb.ru","gb_orders2","za84ededawps","gb_orders2");
 if (!$orders_db) {
     die('Connect Error (' . mysqli_connect_errno() . ') '
             . mysqli_connect_error());
 }
 mysqli_query($orders_db, 'SET NAMES utf8');
 
-$menu_db = mysqli_connect("localhost","gb_orders2","za84ededawps","gb_orders2",8889);
+$menu_db = mysqli_connect("mysql87.1gb.ru","gb_orders2","za84ededawps","gb_orders2");
 if (!$menu_db) {
     die('Connect Error (' . mysqli_connect_errno() . ') '
             . mysqli_connect_error());
